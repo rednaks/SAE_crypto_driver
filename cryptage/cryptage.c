e8bcaa03c0bea6565516ac438a4b208b603c7307
Alexandre BM <s@rednaks.tn>
2015-01-31 15:53:50 +0100

    Part 1 : définition des fonction de cryptage et décryptage cesar et rot13

--- cryptage/cryptage.c
+++ cryptage/cryptage.c
@@ -1,6 +1,69 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
+char cesar(char c) {
+
+  char offset = c <= 'Z' ? 'A' : 'a';
+
+  return ((c - offset + 3)%26) + offset;
+}
+
+char decesar(char c) {
+
+  char offset = c <= 'Z' ? 'A' : 'a';
+
+  return ((c - offset - 3)%26) + offset;
+}
+
+char rot13(char c) {
+  return ((c - 97 + 13)%26)+97;
+}
+
+char derot13(char c) {
+  // car la fonction rot13 est symétrique.
+  return rot13(c);
+}
+
+/*
+   J'ai créé les fonctions cesar, decesar, ro13 et derot13,
+   pour garder le code plus lisible.
+   Ce n'est pas demandé, vous pouvez directement mettre le code
+   dans crypt_cesar, crypt_rot13, ect ...
+*/
+
+static void crypt_cesar(char* buffer, int n) {
+
+  int i = 0;
+
+  for(;i<n;i++){
+    buffer[i] = cesar(buffer[i]);
+  }
+
+}
+
+static void crypt_rot13(char* buffer, int n) {
+  int i = 0;
+
+  for(;i<n;i++){
+    buffer[i] = rot13(buffer[i]);
+  }
+}
+
+static void decrypt_cesar(char* buffer, int n) {
+  int i = 0;
+
+  for(;i<n;i++){
+    buffer[i] = decesar(buffer[i]);
+  }
+}
+
+static void decrypt_rot13(char* buffer, int n) {
+  int i = 0;
+
+  for(;i<n;i++){
+    buffer[i] = derot13(buffer[i]);
+  }
+}
 
 
 static int __init load_module(void) {
