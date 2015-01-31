f7098103e02395ade7d18323307c8370d3cce419
Alexandre BM <s@rednaks.tn>
2015-01-31 16:21:44 +0100

    Part 2 : enregistrement des fichier du driver + fops

--- comm/comm.c
+++ comm/comm.c
@@ -1,17 +1,33 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+
+
+static int _lread(struct file *f, char *buffer, size_t len, loff_t *ptr);
+static int _lwrite(struct file *f, const char *buffer, size_t len, loff_t *ptr);
+
+struct file_operations fops = {
+  owner: THIS_MODULE,
+  write: _lwrite,
+  read: _lread
+};
 
 
 
 static int __init load_module(void) {
   printk("module COMM chargé !\n");
+
+  register_chrdev(150, "comm", &fops);
+
   return 0;
 }
 
 
 static void __exit unload_module(void) {
   printk("module COMM déchargé !\n");
+
+  unregister_chrdev(150, "comm");
 }
 
 module_init(load_module);
